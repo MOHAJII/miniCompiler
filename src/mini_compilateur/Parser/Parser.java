1aeecaa2ab37ae337a25844b8c38f77d0c1fc44d
Pradeep Antil <hmohammediaji@gmail.com>
2024-11-23 09:45:47 +0100

    modify the parser class

--- src/mini_compilateur/Parser/Parser.java
+++ src/mini_compilateur/Parser/Parser.java
@@ -1,4 +1,14 @@
 package mini_compilateur.Parser;
 
+import org.w3c.dom.Node;
+
+import java.util.List;
+
 public class Parser {
+    private final TokenStream tokenStream;
+
+    public Parser(TokenStream tokenStream) {
+        this.tokenStream = tokenStream;
+    }
+
 }
--- src/mini_compilateur/Parser/TokenStream.java
+++ src/mini_compilateur/Parser/TokenStream.java
@@ -1,4 +1,26 @@
 package mini_compilateur.Parser;
 
+import mini_compilateur.Lexer.Token;
+
+import java.util.List;
+
 public class TokenStream {
+    private final List<Token> tokens;
+    private int position = 0;
+
+    public TokenStream(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    public Token getCurrentToken() {
+        return position < tokens.size() ? tokens.get(position) : null;
+    }
+
+    public void advance() {
+        if (position < tokens.size()) position++;
+    }
+
+    public boolean isAtEnd() {
+        return position >= tokens.size();
+    }
 }
